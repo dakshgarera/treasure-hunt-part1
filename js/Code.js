25a0eb46a20f4a7c283249907b91c2ae3bcb143b
dakshgarera <77096274+dakshgarera@users.noreply.github.com>
2021-06-12 13:22:24 +0530

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,22 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+// add code for displaying rest of the names and the hints.
+    fill("white")
+    textSize(15)
+    text("A T E D A S B A",700,150)
+    text("Hint: Stores all imformation!!",700,180)
+
+    fill("white")
+    textSize(15)
+    text("C U T N I F O N",300,300)
+    text("Hint: Performes a particular task!!",300,330)
+}
---
+++ js/System.js
@@ -0,0 +1,13 @@
+class System{
+
+    constructor(){}
+
+    authenticate(actualCode,enteredCode){
+        textSize(50);
+        fill("black");
+        text(code,300,300)
+        //add code to return true if correct answer entered.
+        return true;        
+    }
+
+}
